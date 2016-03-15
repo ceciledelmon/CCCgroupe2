4a2d77b9b73d1252890d626bc2b69aa670f0ecb4
Marine <marine.lemetayer.pro@gmail.com>
2016-03-15 14:07:45 +0100

    Javascript principal

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var leCoon = angular.module('leCoon',[]) ;
 
 // Création du contrôleur
-var createController = function($scope)
+var createController = function($scope, $timeout)
 {
     if (localStorage.getItem("name") === null) {
         $scope.username = "";
@@ -15,17 +15,41 @@ var createController = function($scope)
 		localStorage.name = name;
         $scope.username = localStorage.name;
 	};
-    
-    if(typeof(Storage) !== "undefined") {
-    // Code for localStorage/sessionStorage.
-        console.log('ok');
-    } else {
-    // Sorry! No Web Storage support..
-        console.log('nope');
+
+    var time = 20;
+    $scope.counter = time;
+    $scope.color = "good";
+    $scope.minutes = '0'+Math.floor($scope.counter/60);
+    $scope.secondes = '00';
+    $scope.onTimeout = function(){
+        $scope.counter--;
+        if ($scope.counter > 0) {
+            mytimeout = $timeout($scope.onTimeout,1000);
+            if($scope.counter < time){
+                $scope.minutes = '0'+Math.floor($scope.counter/60);
+                $scope.secondes = $scope.counter%60;
+                if($scope.secondes <10){
+                    $scope.secondes = '0'+$scope.secondes;
+                }
+                if($scope.counter <= 60 && $scope.counter >= 30){
+                    $scope.color = "medium";
+                }else{
+                    if($scope.counter < 30){
+                        $scope.color = "bad";
+                    }
+                }
+            }
+        }
+        else {
+            //Rediriger vers page de fail
+            alert("Time is up!");
+        }
     }
+    var mytimeout = $timeout($scope.onTimeout,1000);
+
 };
   
 // Attachement du contrôleur à l'application, 
 // permet aussi de donner un nom au contrôleur
 // Le contrôleur prend en paramètre le $scope, ce qui lui permet de manipuler tous les models définis dans son rayon d'action
-leCoon.controller('mainController', ["$scope",  createController]) ;
+leCoon.controller('mainController', ["$scope", "$timeout",   createController]) ;
